90d3db53316809060d0bdc935b149545290b8af5
Delphine Pilon <delphine.pilon@icloud.com>
2020-02-11 19:50:10 -0500

    Create drop_tables.sql

---
+++ sql/drop_tables.sql
@@ -0,0 +1,65 @@
+-- Script to drop our tables
+
+DROP TABLE Podcast;
+	
+DROP TABLE b_user;
+	
+DROP TABLE Administrator;
+
+DROP TABLE Artist;
+
+DROP TABLE Playlist;
+	
+DROP TABLE Podcast_Episode;
+	
+DROP TABLE b_subscription;
+
+DROP TABLE Audiofile;
+	
+DROP TABLE Stream;
+
+DROP TABLE Album;
+
+DROP TABLE Song;
+
+DROP TABLE Follows_Playlist;
+
+DROP TABLE Follows_Podcast;
+
+DROP TABLE Follows_Artist;
+
+DROP TABLE Playlist_Has_Song;
+
+	
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

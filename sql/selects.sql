cc48ae8920e0b2499102b74cc594f70ac50b7949
jeniwen <wang.jiawen1@live.ca>
2020-02-23 15:11:28 -0500

    Queries and selects

---
+++ sql/selects.sql
@@ -0,0 +1,62 @@
+-- Use with dv2 -t selects.sql > selects.log 2>&1
+SELECT * FROM Podcast
+LIMIT 5;
+
+
+SELECT * FROM Subscription
+LIMIT 5;
+
+SELECT * FROM Audiofile
+LIMIT 5;
+
+SELECT * FROM b_user
+LIMIT 5;
+
+SELECT * FROM Podcast
+LIMIT 5;
+
+SELECT * FROM Administrator
+LIMIT 5;
+
+SELECT * FROM Artist
+LIMIT 5;
+
+SELECT * FROM Playlist
+LIMIT 5;
+
+SELECT * FROM Podcast_Episode
+LIMIT 5;
+
+SELECT * FROM Stream
+LIMIT 5;
+
+SELECT * FROM Album
+LIMIT 5;
+
+SELECT * FROM Podcast
+LIMIT 5;
+
+SELECT * FROM Song
+LIMIT 5;
+
+SELECT * FROM Follows_Playlist
+LIMIT 5;
+
+SELECT * FROM Follows_Podcast
+LIMIT 5;
+
+SELECT * FROM Follows_Artist
+LIMIT 5;
+
+SELECT * FROM Playlist_Has_Song
+LIMIT 5;
+
+
+
+
+
+
+
+
+
+

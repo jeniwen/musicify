fec5d17a23c7d496e5a7a63e3f09f7dc3aeddef3
AlisaFox <34177605+AlisaFox@users.noreply.github.com>
2020-02-24 18:19:37 -0500

    Update check.sql

--- sql/check.sql
+++ sql/check.sql
@@ -1,1 +1,16 @@
-ALTER TABLE Subscription ADD CONSTRAINT con CHECK (subscription_type >=0 AND subscription_type <=2)
+ALTER TABLE Subscription 
+ADD CONSTRAINT conSub 
+CHECK (subscription_type >=0 AND subscription_type <=2);
+
+ALTER TABLE b_user 
+ADD CONSTRAINT  conUser 
+CHECK (email LIKE ‘%@%.%‘);
+
+ALTER TABLE Playlist 
+ADD CONSTRAINT conPlaylist
+CHECK (accessibility = '1' OR accessibility = '0');
+
+ALTER TABLE Stream 
+ADD CONSTRAINT conStream 
+CHECK (start_time <= '2020‑02‑21‑00.00.00');
+

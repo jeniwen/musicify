dd55ad09f55f44323420448693aa292aa75462ae
Delphine Pilon <delphine.pilon@icloud.com>
2020-03-16 21:22:27 -0400

    further gui work
    this would be our "main" window after login... still need to figure out how to make the first window seperate

--- src/Application/src/Main.java
+++ src/Application/src/Main.java
@@ -1,49 +1,88 @@
 package Application.src;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.GridPane;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-//	public static void main(String[] args) 
-//	{
-//		System.out.println("Hello world!");
-//	}
-
+	public static void main(String[] args) 
+	{
+		launch(args);
+	}
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception 
 	{
-		GridPane root = new GridPane();
-		
-		VBox profileBox = createProfileSection();
-		VBox searchBox = createSearchSection();
-		
-		VBox base = new VBox();
-		
-		base.getChildren().add(profileBox);
-		base.getChildren().add(searchBox);
 
-		root.add(base, 0, 0, 1, 1);
+		VBox root = new VBox();
+		root.getStyleClass().add("outer_box");
+		root.getChildren().add(createProfileSection());
+		root.getChildren().add(createSearchSection());
+		root.getChildren().add(createUtilitiesBar());
 		
-		Scene scene = new Scene(root);
-		scene.setFill(Color.GAINSBORO);
+		Scene scene = new Scene(root, 500, 500);
+		scene.getStylesheets().add("Application/src/style.css");
+		
+		primaryStage.setTitle("Application");
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 	
+	private HBox createUtilitiesBar() 
+	{
+		HBox utilitiesBar = new HBox();
+		utilitiesBar.getStyleClass().add("inner_box");
+		utilitiesBar.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2))));
+		
+		Button logoutButton = new Button("Log Out");
+		logoutButton.getStyleClass().add("button");
+		logoutButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				// how to logout
+			}
+			
+		});
+		
+		Button quitButton = new Button("Quit");
+		quitButton.getStyleClass().add("button");
+		quitButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				System.exit(0);
+			}
+			
+		});
+		
+		utilitiesBar.getChildren().addAll(logoutButton, quitButton);
+		
+		return utilitiesBar;
+	}
+
 	private VBox createProfileSection()
 	{
 		VBox profileBox = new VBox();
 		
-		profileBox.setMinSize(500, 300);
-		profileBox.getChildren().add(new Text("profile section"));
+		profileBox.getStyleClass().add("inner_box");
+		profileBox.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2))));
+		Label title = new Label("Profile Section");
+		title.getStyleClass().add("label");
+		profileBox.getChildren().add(title);
 		
 		return profileBox;
 	}
@@ -52,8 +91,11 @@ public class Main extends Application {
 	{
 		VBox searchBox = new VBox();
 		
-		searchBox.setMinSize(500, 300);
-		searchBox.getChildren().add(new Text("search section"));
+		searchBox.getStyleClass().add("inner_box");
+		searchBox.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(2))));
+		Label title = new Label("Search Section");
+		title.getStyleClass().add("label");
+		searchBox.getChildren().add(title);
 		
 		return searchBox;
 	}
